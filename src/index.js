b060c9b289a59dbef56960fd06c3a15cb4090f35
eden park <eden.park@georgebrown.ca>
2024-02-06 11:18:24 -0500

    including application source

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Ensure you have this file or remove this line
+import App from './App'; // Ensure you have App.js or adjust accordingly
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
